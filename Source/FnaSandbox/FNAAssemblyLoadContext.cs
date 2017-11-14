84e6dc0bc2daca62717fd6a805d4ca213b7fdd29
smack0007 <zachary.snow@gmail.com>
2017-11-14 21:02:35 +0100

    Cornflower blue.

---
+++ Source/FnaSandbox/FNAAssemblyLoadContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace FnaSandbox
+{
+    public class FNAAssemblyLoadContext : AssemblyLoadContext
+    {
+        internal void Init()
+        {
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), Environment.Is64BitProcess ? "x64" : "x86");
+            var dlls = Directory.EnumerateFiles(path, "*.dll");
+
+            foreach (var dll in dlls)
+            {
+                this.LoadUnmanagedDllFromPath(dll);
+            }
+        }
+
+        protected override Assembly Load(AssemblyName assemblyName) => null;
+    }
+}
---
+++ Source/FnaSandbox/SandboxGame.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FnaSandbox
+{
+    public class SandboxGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public SandboxGame()
+        {
+            this.graphics = new GraphicsDeviceManager(this);
+            this.Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            this.spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+        
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            this.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+
+        public static void Main(string[] args)
+        {
+            new FNAAssemblyLoadContext().Init();
+
+            using (var game = new SandboxGame())
+                game.Run();
+        }
+    }
+}
